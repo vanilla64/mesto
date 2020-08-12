ef9ba0ec1dd5cfc6760c3a1356e59361e09a13d8
vanilla64 <62620124+vanilla64@users.noreply.github.com>
2020-08-12 21:05:41 +0300

    feat: install babel

--- src/index.js
+++ src/index.js
@@ -115,3 +115,11 @@ editProfileBtn.addEventListener('click', () => handleEditProfile.open())
 addCardBtn.addEventListener('click', () => handleAddCard.open())
 
 console.log('Its ALIVE!')
+
+const numbers = [2, 3, 5];
+
+// Стрелочная функция. Не запнётся ли на ней Internet Explorer?
+const doubledNumbers = numbers.map(number => number * 2);
+
+console.log(doubledNumbers); // 4, 6, 10
+console.log('god hate us')
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,15 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules/'
+      }
+    ]
   }
 }
