91643bbf4c873a3ae3577d67aba52d3628912602
vanilla64 <62620124+vanilla64@users.noreply.github.com>
2020-08-12 22:41:38 +0300

    feat: install html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-const path = require('path')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // подключили плагин
 
 module.exports = {
   entry: { main: './src/index.js' },
@@ -6,14 +7,19 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js'
   },
-
   module: {
     rules: [
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        exclude: '/node_modules/'
+        exclude: /node_modules/
       }
     ]
-  }
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html' // путь к файлу index.html
+    })
+  ]
 }
